03dc5b68065bbbcf9ceb3b921d3746a4f6ed3448
Abed Nganga <ngangaabed@gmail.com>
2023-06-16 08:35:53 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Write your solution in this file!
+
+var customerName = 'bob';  //let doesnt work
+ 
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+    bestCustomer ='not bob';
+}
+
+function overwriteBestCustomer(newCustomer){
+    bestCustomer = newCustomer;
+}
+
+
+function changeLeastFavoriteCustomer(){
+    const leastFavouriteCustomer ='jojo'  //cant reasign a const variable
+    leastFavouriteCustomer = 'mark';
+};
